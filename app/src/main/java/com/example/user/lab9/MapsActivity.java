d272d279af4e5aab230bf13bed1fa748d3745714
USER <zetymedinaa@gmail.com>
2017-11-16 10:45:17 +0800

    Initial commit

---
+++ app/src/main/java/com/example/user/lab9/MapsActivity.java
@@ -0,0 +1,85 @@
+package com.example.user.lab9;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import com.google.android.gms.maps.CameraUpdateFactory;
+
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+        String provider = locationManager.getBestProvider(criteria, true);
+        LocationListener locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                showCurrentLocation(location);
+            }
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+            }
+            @Override
+            public void onProviderEnabled(String provider) {
+            }
+            @Override
+            public void onProviderDisabled(String provider) {
+            }
+        };
+        locationManager.requestLocationUpdates(provider, 2000, 0, locationListener);
+        Location location = locationManager.getLastKnownLocation(provider);
+        if(location != null) {
+            showCurrentLocation(location);
+        }
+    }
+
+    private void showCurrentLocation(Location location){
+        mMap.clear();
+        LatLng currentPosition = new LatLng(location.getLatitude(),location.getLongitude());
+        mMap.addMarker(new MarkerOptions()
+                .position(currentPosition)
+                .snippet("Lat: " + location.getLatitude() + ", Lng: " + location.getLongitude())
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher))
+                .flat(true)
+                .title("I'm here!"));
+// Zoom in, animating the camera.
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentPosition, 18));
+    }
+}
